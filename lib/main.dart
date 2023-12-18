aae6bba57342b89b5aaba161d10c7bfc2eb12b03
Nicolas Lopez <unacorbatanegra@gmail.com>
2023-12-18 09:44:24 -0300

    pubspect

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: AppTheme.appThemeData,
-        home: LandingPage(),
+        home: const LandingPage(),
       ),
     );
   }
