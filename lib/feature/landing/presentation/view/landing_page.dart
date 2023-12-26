66d93897335b6037f7a509abfec5f465cdae5873
Matias Ramirez <ramirezmatias946@gmail.com>
2023-12-25 21:57:06 -0300

    Refactor landing page layout and add speaker cards

--- lib/feature/landing/presentation/view/landing_page.dart
+++ lib/feature/landing/presentation/view/landing_page.dart
@@ -1,46 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_conf_web/feature/landing/presentation/widget/leyends_widget.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:lottie/lottie.dart';
+import 'package:flutter_conf_web/feature/landing/models/speaker_model.dart';
+import 'package:flutter_conf_web/feature/landing/presentation/view/about_view.dart';
+import 'package:flutter_conf_web/feature/landing/presentation/widget/custom_drawer.dart';
+import 'package:flutter_conf_web/feature/landing/presentation/widget/navigation_bar.dart';
+import 'package:flutter_conf_web/feature/landing/presentation/widget/speaker_card.dart';
+import 'package:flutter_conf_web/gen/assets.gen.dart';
 
 class LandingPage extends StatelessWidget {
-  const LandingPage({super.key});
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  LandingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.centerLeft,
-            end: Alignment.centerRight,
-            colors: [
-              Color(0xFF3F4E61),
-              Color(0xFF072436),
-            ],
+      key: scaffoldKey,
+      endDrawer: const CustomDrawer(),
+      body: Column(
+        children: [
+          CustomNavigationBar(
+            scaffoldKey: scaffoldKey,
           ),
-        ),
-        child: Stack(
-          children: [
-            const Positioned.fill(
-              top: 80,
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: LeyendsWidget(),
-              ),
+          Expanded(
+            child: ListView(
+              children: [
+                Assets.images.banner1.svg(
+                  width: size.width,
+                ),
+                const SizedBox(height: 50),
+                const AboutView(),
+                const SizedBox(height: 50),
+                const Text(
+                  'Speakers',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 400,
+                  child: GridView.count(
+                    crossAxisCount: 1,
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    children: List.generate(
+                      speakers.length,
+                      (index) {
+                        final speaker = speakers[index];
+                        return SpeakerCard(
+                          speaker: speaker,
+                        );
+                      },
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 50),
+              ],
             ),
-            Positioned.fill(
-              child: Align(
-                alignment: Alignment.center,
-                child: Lottie.asset(
-                    'assets/animation/loading_flutter_logo.json',
-                    height: 20.sw),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
