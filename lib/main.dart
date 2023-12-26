fb66221fb07c6eaa0ddc3e4e7cf0746a0d54d20e
Matias Ramirez <ramirezmatias946@gmail.com>
2023-12-25 21:56:45 -0300

    Remove unused import statement

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_conf_web/core/themes/colors.dart';
-import 'package:flutter_conf_web/core/themes/themes.dart';
 import 'package:flutter_conf_web/feature/landing/presentation/view/landing_page.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
