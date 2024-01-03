a1682d39ab184939117fc657f6399e3b1be9f66a
Matias Ramirez <ramirezmatias946@gmail.com>
2024-01-03 16:40:37 -0300

    Add router and team view to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_conf_web/core/router/router.dart';
 import 'package:flutter_conf_web/core/themes/colors.dart';
 import 'package:flutter_conf_web/feature/landing/presentation/view/landing_page.dart';
+import 'package:flutter_conf_web/feature/landing/presentation/view/team_view.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final _router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => LandingPage(),
+      ),
+      GoRoute(
+        path: '/team',
+        builder: (context, state) => const TeamView(),
+      ),
+    ],
+    errorBuilder: (context, state) => Scaffold(
+      body: Center(
+        child: Text(
+          'Page not found',
+          style: Theme.of(context).textTheme.headline1,
+        ),
+      ),
+    ),
+    initialLocation: '/',
+  );
 
   // This widget is the root of your application.
   @override
@@ -18,7 +43,8 @@ class MyApp extends StatelessWidget {
     return ScreenUtilInit(
       designSize: const Size(360, 690),
       minTextAdapt: true,
-      child: MaterialApp(
+      child: MaterialApp.router(
+        routerConfig: _router,
         title: 'Flutter Demo',
         debugShowCheckedModeBanner: false,
         theme: _buildTheme(Brightness.light),
@@ -27,7 +53,6 @@ class MyApp extends StatelessWidget {
           Locale('en'),
           Locale('es'),
         ],
-        home: LandingPage(),
       ),
     );
   }
