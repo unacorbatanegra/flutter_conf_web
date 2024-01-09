eb0f6086f24b25d4daff6107cf523213c84b5d72
Matias Ramirez <ramirezmatias946@gmail.com>
2024-01-09 20:55:09 -0300

    Refcator of animation code making it responsive

--- lib/app/widgets/animated_banner_widget.dart
+++ lib/app/widgets/animated_banner_widget.dart
@@ -2,6 +2,7 @@ import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_conf_web/core/themes/colors.dart';
 import 'package:flutter_conf_web/gen/assets.gen.dart';
+import 'package:flutter_conf_web/l10n/l10n.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -11,132 +12,239 @@ class AnimatedBannerWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Container(
       color: AppColors.secondaryColor,
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: 90.dm,
-        child: Stack(
+      child: size.width > 900
+          ? const _AnimationDesktop()
+          : const _AnimationMobile(),
+    );
+  }
+}
+
+class _AnimationDesktop extends StatelessWidget {
+  const _AnimationDesktop();
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    final size = MediaQuery.of(context).size;
+    return Row(
+      children: [
+        Assets.animation.bubblesSides.lottie(),
+        const Spacer(),
+        if (size.width > 1400)
+          WideBanner(l10n: l10n)
+        else
+          LongBanner(l10n: l10n),
+        const Spacer(),
+        Assets.animation.bubblesSides.lottie(),
+      ],
+    );
+  }
+}
+
+class LongBanner extends StatelessWidget {
+  const LongBanner({
+    super.key,
+    required this.l10n,
+  });
+
+  final AppLocalizations l10n;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          l10n.eventDate,
+          style: GoogleFonts.inter(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            fontSize: 12.sp,
+          ),
+        ),
+        Text(
+          l10n.eventPlace,
+          style: GoogleFonts.inter(
+            fontWeight: FontWeight.w200,
+            color: Colors.white,
+            fontSize: 8.sp,
+          ),
+          textAlign: TextAlign.end,
+        ),
+        const SizedBox(width: 20),
+        Assets.animation.flutterHeartbeat.lottie(),
+        const SizedBox(width: 20),
+        Text(
+          '${l10n.eventNameTitle} ${l10n.eventNameSubtitle}',
+          style: GoogleFonts.inter(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            fontSize: 12.sp,
+          ),
+        ),
+        Text(
+          l10n.eventCountry,
+          style: GoogleFonts.inter(
+            fontWeight: FontWeight.w200,
+            color: Colors.white,
+            fontSize: 8.sp,
+          ),
+          textAlign: TextAlign.start,
+        ),
+      ],
+    );
+  }
+}
+
+class WideBanner extends StatelessWidget {
+  const WideBanner({
+    super.key,
+    required this.l10n,
+  });
+
+  final AppLocalizations l10n;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Column(
           children: [
-            Positioned(
-              left: 2.w,
-              child: Align(
-                alignment: Alignment.center,
-                child: Assets.animation.bubblesSides.lottie(
-                  height: 90.dm,
-                  fit: BoxFit.cover,
-                ),
+            Text(
+              l10n.eventDate,
+              style: GoogleFonts.inter(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontSize: 12.sp,
               ),
             ),
-            Positioned(
-              right: 2.w,
-              child: Align(
-                alignment: Alignment.center,
-                child: Assets.animation.bubblesSides.lottie(
-                  fit: BoxFit.cover,
-                  height: 90.dm,
-                ),
+            Text(
+              l10n.eventPlace,
+              style: GoogleFonts.inter(
+                fontWeight: FontWeight.w200,
+                color: Colors.white,
+                fontSize: 8.sp,
               ),
-            ),
-            Positioned(
-              left: 1,
-              top: -45.dm,
-              right: 1,
-              child: const _RotateWheelAnimation(),
-            ),
-            Positioned(
-              bottom: 40.dm,
-              left: 90.dm,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text(
-                    AppLocalizations.of(context)!.eventDate,
-                    style: GoogleFonts.inter(
-                      color: Colors.white,
-                      fontSize: 10.dm,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 2,
-                  ),
-                  Text(
-                    AppLocalizations.of(context)!.eventPlace,
-                    style: GoogleFonts.inter(
-                      color: Colors.white,
-                      fontSize: 7.dm,
-                      fontWeight: FontWeight.w200,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Positioned(
-              bottom: 40.dm,
-              right: 90.dm,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text(
-                        AppLocalizations.of(context)!.eventNameTitle,
-                        style: GoogleFonts.inter(
-                          color: Colors.white,
-                          fontSize: 10.dm,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        AppLocalizations.of(context)!.eventNameSubtitle,
-                        style: GoogleFonts.inter(
-                          color: Colors.white,
-                          fontSize: 10.dm,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Text(
-                    AppLocalizations.of(context)!.eventCountry,
-                    style: GoogleFonts.inter(
-                      color: Colors.white,
-                      fontSize: 7.dm,
-                      fontWeight: FontWeight.w200,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Positioned(
-              top: 25.dm,
-              bottom: 25.dm,
-              left: 90.dm,
-              right: 90.dm,
-              child: Assets.animation.flutterHeartbeat.lottie(),
-            ),
-            Positioned(
-              top: 37.dm,
-              left: 100.dm,
-              child: Align(
-                alignment: Alignment.center,
-                child: Assets.animation.flagStripes
-                    .lottie(height: 90.dm, width: 40.dm),
-              ),
+              textAlign: TextAlign.end,
             ),
           ],
         ),
-      ),
+        const SizedBox(width: 20),
+        Assets.animation.flutterHeartbeat.lottie(),
+        const SizedBox(width: 20),
+        Column(
+          children: [
+            Text(
+              '${l10n.eventNameTitle} ${l10n.eventNameSubtitle}',
+              style: GoogleFonts.inter(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontSize: 12.sp,
+              ),
+            ),
+            Text(
+              l10n.eventCountry,
+              style: GoogleFonts.inter(
+                fontWeight: FontWeight.w200,
+                color: Colors.white,
+                fontSize: 8.sp,
+              ),
+              textAlign: TextAlign.start,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _AnimationMobile extends StatelessWidget {
+  const _AnimationMobile();
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SizedBox(
+          height: 100.h,
+          width: size.width,
+          child: Assets.animation.bubblesSides.lottie(),
+        ),
+        Column(
+          children: [
+            Column(
+              children: [
+                Text(
+                  context.l10n.eventDate,
+                  style: GoogleFonts.inter(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 12.sp,
+                  ),
+                ),
+                Text(
+                  context.l10n.eventPlace,
+                  style: GoogleFonts.inter(
+                    fontWeight: FontWeight.w200,
+                    color: Colors.white,
+                    fontSize: 8.sp,
+                  ),
+                  textAlign: TextAlign.end,
+                ),
+              ],
+            ),
+            SizedBox(
+              child: Assets.animation.flutterHeartbeat.lottie(
+                width: 200.w,
+                height: 200.h,
+              ),
+            ),
+            Column(
+              children: [
+                Text(
+                  '${context.l10n.eventNameTitle} ${context.l10n.eventNameSubtitle}',
+                  style: GoogleFonts.inter(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 12.sp,
+                  ),
+                ),
+                Text(
+                  context.l10n.eventCountry,
+                  style: GoogleFonts.inter(
+                    fontWeight: FontWeight.w200,
+                    color: Colors.white,
+                    fontSize: 8.sp,
+                  ),
+                  textAlign: TextAlign.start,
+                ),
+              ],
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 100.h,
+          child: Assets.animation.bubblesSides.lottie(),
+        ),
+      ],
     );
   }
 }
 
 class _RotateWheelAnimation extends StatefulWidget {
-  const _RotateWheelAnimation();
+  final double? height;
+  final double? width;
+
+  const _RotateWheelAnimation({
+    this.height,
+    this.width,
+  });
 
   @override
   State<_RotateWheelAnimation> createState() => __RotateWheelAnimationState();
@@ -158,6 +266,7 @@ class __RotateWheelAnimationState extends State<_RotateWheelAnimation>
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return RotationTransition(
       turns: Tween(begin: 0.0, end: 1.0).animate(_controller),
       child: DottedBorder(
@@ -167,8 +276,8 @@ class __RotateWheelAnimationState extends State<_RotateWheelAnimation>
         strokeCap: StrokeCap.round,
         dashPattern: [11.dm, 20.dm],
         child: Container(
-          height: 215.dm,
-          width: 215.dm,
+          height: widget.height,
+          width: widget.width,
           decoration: const BoxDecoration(
             shape: BoxShape.circle,
           ),
